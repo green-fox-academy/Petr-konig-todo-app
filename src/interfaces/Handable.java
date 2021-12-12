b8ad847b439a8feef1b854ff9fb43be32638c510
Petr-konig <peta.konig@seznam.cz>
2021-12-12 23:04:48 +0100

    Interface package

---
+++ src/interfaces/Handable.java
@@ -0,0 +1,3 @@
+public interface Handable {
+    void handleArgs(String[] args) throws NoTaskProvidedException, NoIndexProvidedException, GreaterIndexException, NotANumberException, UnsupportedArgumentException, NoIndexProvidedCheckException, NotANumberCheckException, GreaterIndexCheckException;
+}
