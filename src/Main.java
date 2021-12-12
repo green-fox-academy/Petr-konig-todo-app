dde76357ddb0e6af6788d63c707ca18d37f08983
Petr-konig <peta.konig@seznam.cz>
2021-12-12 23:06:58 +0100

    Main in final form

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import exceptions.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        ToDo app = new ToDo(args);
-        app.handleArgs();
+        ToDo app = new ToDo();
+
+        try {
+            app.handleArgs(args);
+        } catch (NoTaskProvidedException | NoIndexProvidedException | GreaterIndexException | NotANumberException | NotANumberCheckException | GreaterIndexCheckException | NoIndexProvidedCheckException e) {
+            System.out.println(e.getMessage());
+        } catch (UnsupportedArgumentException e) {
+            System.out.println(e.getMessage());
+            System.out.println();
+            app.printCommands();
+        }
     }
 }
